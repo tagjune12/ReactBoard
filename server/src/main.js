cb84de68acb6eb0160dc14d8fd5c619f5268917e
tagjune12 <tagjune12@gmail.com>
2022-08-07 00:10:03 +0900

    feat: static file 제공을 위한 설정 완료

--- server/src/main.js
+++ server/src/main.js
@@ -5,6 +5,10 @@ import bodyParser from 'koa-bodyparser';
 import db from 'mongoose';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
 
 const app = new Koa();
 const router = new Router();
@@ -23,6 +27,14 @@ app.use(bodyParser());
 app.use(jwtMiddleware)
 // app인스턴스에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../client/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory })
+  }
+});
 
 
 const port = PORT || 4000;
